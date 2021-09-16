2cf27d27f795a74d246608b4aa4edefd80d17c53
Sebastian <sa224ny@student.lnu.se>
2021-09-16 11:03:15 +0200

    Add: examples for Grammar and Tokenizer

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import Tokenizer from './modules/Tokenizer.js'
+import Grammar from './modules/Grammar/Grammar.js'
 import WordAndDot from './modules/Grammar/WordAndDot.js'
 import Arithmetic from './modules/Grammar/Arithmetic.js'
 import MaximalMunch from './modules/Grammar/MaximalMunch.js'
@@ -6,6 +7,7 @@ import Exclamation from './modules/Grammar/Exclamation.js'
 
 export {
   Tokenizer,
+  Grammar,
   WordAndDot,
   Arithmetic,
   MaximalMunch,
